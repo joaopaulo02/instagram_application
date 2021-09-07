66eec17ac835efe43057099376b2d2c9a64584c4
joaopaulo02 <jpalonso0209@gmail.com>
2021-09-07 18:57:39 -0300

    serve the css of the search_bar as static

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
- 
+
+// serve the css as static
+app.use(express.static('search_bar'))
+
 // routes 
 app.get('/search-bar', function (req, res) {
     res.sendFile(__dirname + '/search_bar/search_bar.html')
 })
- 
+
 app.listen(3000)
 console.log('Server running at localhost:3000')
