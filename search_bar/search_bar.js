141a5b84e774fc91658e43ab13aa259e9bcb480e
joaopaulo02 <jpalonso0209@gmail.com>
2021-09-07 19:43:21 -0300

    small changes.

--- search_bar/search_bar.js
+++ search_bar/search_bar.js
@@ -1,5 +1,5 @@
 function storeTag() {
     var tag = document.querySelector('#tag').value 
-    alert(tag)
-    tag_link = `https://www.instagram.com/explore/tags/${tag_link}}/`
+    tag_link = `https://www.instagram.com/explore/tags/${tag}}/`
+    window.location.href = 'tag_link'
 }
