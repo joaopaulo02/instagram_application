4d1b835dc2632d61f158b4d48d41cf7763474911
joaopaulo02 <jpalonso0209@gmail.com>
2021-09-08 17:02:48 -0300

    function that remove space from words added.

--- search_bar/search_bar.js
+++ search_bar/search_bar.js
@@ -1,5 +1,11 @@
 function pageRedirect() {
-    var tag = document.querySelector('#tag').value 
-    tagLink = `https://www.instagram.com/explore/tags/${tag}/`
+    var tag = document.querySelector('#tag').value
+
+    // if the tag has space, remove it.
+    if(!tag.match(/^(\s)+$/)) {
+        var noSpacedTag = tag.replace(/ /g,'')
+    }
+
+    tagLink = `https://www.instagram.com/explore/tags/${noSpacedTag}/`
     window.location.replace(tagLink)
 }
