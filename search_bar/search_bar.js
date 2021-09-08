555a27805a55d3fbfe07de0660d1b8ff20d22203
joaopaulo02 <jpalonso0209@gmail.com>
2021-09-08 18:55:54 -0300

    tag formated.

--- search_bar/search_bar.js
+++ search_bar/search_bar.js
@@ -4,8 +4,22 @@ function pageRedirect() {
     // if the tag has space, remove it.
     if(!tag.match(/^(\s)+$/)) {
         var noSpacedTag = tag.replace(/ /g,'')
+        tagLink = `https://www.instagram.com/explore/tags/${noSpacedTag}/`
+        window.location.replace(tagLink)
     }
 
-    tagLink = `https://www.instagram.com/explore/tags/${noSpacedTag}/`
-    window.location.replace(tagLink)
+    // if the tag has the 'hashtag' simbol, remove it.
+    if(tag.match('#')) {
+        var hashtagTag = tag.replace('#','')
+        tagLink = `https://www.instagram.com/explore/tags/${hashtagTag}/`
+        window.location.replace(tagLink)
+    }
+
+    // if the tag has the 'hashtag' simbol AND space, remove both.
+    if(tag.match('#') && !tag.match(/^(\s)+$/)) {
+        var formatedTag = tag.replace('#','').replace(/ /g,'')
+        tagLink = `https://www.instagram.com/explore/tags/${formatedTag}/`
+        window.location.replace(tagLink)
+    }
+
 }
